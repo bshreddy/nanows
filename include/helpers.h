76e0c28b5b0359158fa35d3acc8acbfb8add2898
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-07-07 21:16:32 +0530

    Added string helpers functions

---
+++ include/helpers.h
@@ -0,0 +1,7 @@
+#ifndef _HELPERS_H
+#define _HELPERS_H 1
+
+char *ltrim(char *s);
+char *rtrim(char *s);
+char *trim(char *s);
+#endif
---
+++ slib/helpers.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <string.h>
+
+#include "helpers.h"
+
+char *ltrim(char *s) {
+    while(isspace(*s)) s++;
+    return s;
+}
+
+char *rtrim(char *s) {
+    char* back = s + strlen(s);
+    while(isspace(*--back));
+    *(back+1) = '\0';
+    return s;
+}
+
+char *trim(char *s) {
+    return rtrim(ltrim(s)); 
+}
