89c9cddfd2f66f48c38c44f0ca49d7db21fc6223
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-07-26 23:13:19 +0530

    Added docs for server.c

--- src/server.c
+++ src/server.c
@@ -1,3 +1,18 @@
+/**
+ * @file src/server.c
+ * @brief Web Server main source file and entry point.
+ *
+ * Implements the main function of ***Nano Web Server.*** It is a simple, multi-threaded, web server
+ * written using POSIX Sockets. Uses a process-driven approach and creates a new thread for each
+ * request. This web server can only host simple static webpages with no server-size processing. As
+ * of v2, it does not conform to any HTTP protocol. This is a learning project and is not intended
+ * to be used in a production environment.
+ *
+ * @author Sai Hemanth Bheemreddy (@SaiHemanthBR)
+ * @copyright MIT License; Copyright (c) 2021 Sai Hemanth Bheemreddy
+ * @bug No known bugs.
+ */
+
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
